12fd7783fcbf9ce2df45bb1fe6f276ec4cad79e0
Guillaume de Granrut <guillaume.degranrut@gmail.com>
2021-06-19 14:25:35 +0200

    FEAT: TOTEST ajout distribution cartes,

--- model/PartieModel.py
+++ model/PartieModel.py
@@ -15,6 +15,7 @@ class Partie:
         rd.shuffle(self.sabot)
         logger.debug(f"Sabot : {self.sabot}")
 
+
     def set_nb_joueur(self, nb_joueur: int):
         """
         :param nb_joueur: nombre de joueur de la partie
@@ -25,6 +26,37 @@ class Partie:
         if nb_joueur in range(1, 6):
             logger.info("nombre de joueur correct")
             self.nb_joueur = nb_joueur
+            self.list_mains = [[] * nb_joueur]
         else:
             logger.warning("nombre de joueur incorrect")
             raise ValueError("Nombre de joueur incorrect")
+
+    def set_taille_main(self):
+        """
+        Configure la taille des mains selon le nombre de joueurs
+        8 cartes si 1 joueur
+        7 cartes si 2 joueurs
+        6 cartes si 3 joueurs ou plus
+        :return: None
+        :raises: AttributeError si le nombre de joueur n'est pas défini
+        """
+        try:
+            self.taille_main = 6 + (self.nb_joueur <= 2) + (self.nb_joueur == 1)
+        except:
+            raise AttributeError("le nombre de joueur n'est pas défini")
+
+    def complete_main(self, id_joueur: int):
+        """
+        Distribue dans la main du joueur id_joueur le nombre de carte nécessaire pour qu'il ait une main valide
+        (cf taille_main : variable globale de taille de main, selon le nombre de joueur)
+        :param id_joueur: id du joueur (indice du jeu à compléter)
+        :return:
+        """
+        try:
+            self.taille_main
+            self.nb_joueur
+        except:
+            self.set_taille_main()
+        assert 0 <= id_joueur <= self.nb_joueur
+        for _ in range(min(self.taille_main - len(self.list_mains[id_joueur]), len(self.sabot))):
+            self.list_mains[id_joueur].append(self.sabot.pop())
